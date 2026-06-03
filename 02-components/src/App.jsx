0cb6d2bc97d3fee039f5d7604df179771c12bf02
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-06-03 13:53:24 +0530

    Add 02-components folder

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './components/card'
+import Navbar from './components/navbar'
+
+const App = () => {
+  return (
+    <>
+
+    <Navbar />
+    <Navbar />
+    <Navbar />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div id="parent" >
+      <div className="card">
+        <h1>Hritk Srivastava</h1>
+        <p>Hi i am a Gen AI Full Stack Developer</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
