4ca259fad5da19d070b5578bdca3682fc2b9e2f6
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-06-03 12:12:59 +0530

    Initial React setup

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello from H1</h1>
+      <h2>Hello from H2</h2>
+      <div className="div1">This is a div</div>
+      <h2>heyyyyy</h2>
+    </>
+  )
+}
+
+export default App
