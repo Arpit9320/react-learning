77b6988e261d3f65b39af8e060cef2d270c8b50b
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-06-12 02:24:06 +0530

    Api through Axios

--- 11-apicalling/src/App.jsx
+++ 11-apicalling/src/App.jsx
@@ -1,21 +1,28 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 const App = () => {
 
-  const getdata = async () =>{
-    
-    const reponse = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const dets = await reponse.json()
-    console.log(dets)
+  
 
+  let [dets, setDets] = useState([])
+  const getdata = async ()=>{
+    const response = await axios.get('https://picsum.photos/v2/list');
+
+    setDets(response.data)
+    // console.log("this is dets", dets)
   }
 
-
-
+  
 
   return (
     <div>
       <button onClick={getdata}>Get Data</button>
+
+      {dets.map((obj)=>{
+        return <img src={obj.download_url} alt="" loading='lazy'  style={{width: '300px', height: '200px', objectFit: 'cover'}}/>
+      })}
+
     </div>
 
   )
